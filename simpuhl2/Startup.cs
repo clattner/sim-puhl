88ffe6f1d4ed375500f89b72a5e1ce14b6968b56
clattner <chris.lattner@live.com>
2014-07-10 19:27:13 -0500

    First Git commit

---
+++ simpuhl2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(simpuhl2.Startup))]
+namespace simpuhl2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
